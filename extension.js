9fb20df561cb6dd9b4d8909eac9cdfb8686c6323
Liv Erickson <livi.erickson@gmail.com>
2023-12-21 10:26:07 -0500

    change to use PanelButton instead of St Button to prepare for putting in a menu UI

--- extension.js
+++ extension.js
@@ -3,20 +3,25 @@
 // Adds a button to the GNOME taskbar at the top  of the OS 
 // Tested on GNOME v42, Ubuntu 22.04
 
-const {St, Clutter} = imports.gi;
+const { St, Clutter } = imports.gi;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
 const SCHEMA_STRING = 'org.gnome.shell.extensions.elgato-light';
 
 // Define objects for the window
-let panelButton;
+let uiWindow, indicatorButton, indicatorButtonText;
 
 // Variables for controlling the light
 let lightOn = false;
 
+let settings;
+
+// Get the saved settings from the light
 function getLightSettingsFromSchema() {
   let GioSchemaSource = Gio.SettingsSchemaSource;
   let schemaSource = GioSchemaSource.new_from_directory(
@@ -28,52 +33,60 @@ function getLightSettingsFromSchema() {
   if (!schemaObj) {
     log('Error finding saved settings schema');
   }
-  return new Gio.Settings({ settings_schema : schemaObj });
+  return new Gio.Settings({ settings_schema: schemaObj });
 }
 
-function toggleLight () {
+function buildUI() {
+  let brightness = new PopupMenu.PopupMenuItem('Brightness');
+  brightness.add_child(new St.Label({ text: 'Brightness: ' + settings.get_int('bright') }));
+  indicatorButton.menu.addMenuItem(brightness);
+}
+
+function toggleLight() {
 
   // Run `journalctl -f -o cat /usr/bin/gnome-shell in terminal window to view log output
   log('Calling toggleLight...');
-  
-
-  if(lightOn) {
+  if (lightOn) {
     lightOn = false;
     GLib.spawn_command_line_sync('keylight-control --bright 0');
+    indicatorButtonText.set_text('On');
   } else {
     lightOn = true;
     GLib.spawn_command_line_sync('keylight-control --bright 50');
+    indicatorButtonText.set_text('Off');
   }
+
 }
 
-function init () {
+function init() {
+  // Load the settings file from extension schema
+  settings = getLightSettingsFromSchema();
+  log('Light IP Address: ' + settings.get_string('light-ip-address'));
+}
+
+function enable() {
     // Create a toggle button with "Elgato" text
-    // TODO: Make panel with additional controls
-    // For now, just a toggle
-    panelButton = new St.Button({
-        style_class : 'panel-button'
-    });
-    let panelButtonText = new St.Label({
-        text : 'Elgato',
-        y_align: Clutter.ActorAlign.CENTER,
-    });
+  // TODO: Make panel with additional controls
+  // For now, just a toggle
+  indicatorButton = new PanelMenu.Button({
+    style_class: 'panel-button'
+  });
+  indicatorButtonText = new St.Label({
+    text: 'Elgato',
+    y_align: Clutter.ActorAlign.CENTER,
+  });
 
-    // Make button respond as a toggle, add text, connect press event
-    panelButton.set_toggle_mode(true);
-    panelButton.set_child(panelButtonText);
-    panelButton.connect('button-press-event', toggleLight);
+  //Make button respond as a toggle, add text, connect press event
+  //indicatorButton.set_toggle_mode(true);
+  indicatorButton.add_child(indicatorButtonText);
+  indicatorButton.connect('button-press-event', toggleLight); 
+
+
+  // Add the button to the panel
+  Main.panel.addToStatusArea('indicator', indicatorButton, 1);
 }
 
-function enable () {
-    // Add the button to the panel
-    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
-
-    // Load the settings file from extension schema
-    let settings = getLightSettingsFromSchema();
-    log('Light IP Address: ' + settings.get_string('light-ip-address'));
-}
-
-function disable () {
-    // Remove the added button from panel
-    Main.panel._rightBox.remove_child(panelButton);
+function disable() {
+  // Remove the added button from panel
+  indicatorButton.destroy();
 }
