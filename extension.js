3d0b2532aa532050a6c65874c691e8923ee0421a
Liv Erickson <livi.erickson@gmail.com>
2023-12-21 13:02:35 -0500

    Add sliders for brightness and temperature. Temperature not yet implemented

--- extension.js
+++ extension.js
@@ -4,22 +4,74 @@
 // Tested on GNOME v42, Ubuntu 22.04
 
 const { St, Clutter } = imports.gi;
-const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
 const Gio = imports.gi.Gio;
+
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const Slider = imports.ui.slider;
 
 const SCHEMA_STRING = 'org.gnome.shell.extensions.elgato-light';
 
 // Define objects for the window
-let uiWindow, indicatorButton, indicatorButtonText;
+let uiWindow, indicatorButton, indicatorButtonText, toggleButton, toggleLabel;
 
 // Variables for controlling the light
 let lightOn = false;
-
 let settings;
+
+const UIWindow = GObject.registerClass(
+  class UIWindow extends PanelMenu.Button {
+    _init() {
+      super._init(0);
+
+      // Set up the indicator button
+      indicatorButtonText = new St.Label({
+        text: 'Elgato',
+        y_align: Clutter.ActorAlign.CENTER,
+      });
+
+      this.add_child(indicatorButtonText);
+
+      toggleButton = new PopupMenu.PopupMenuItem('Light Status:');
+      toggleLabel = new St.Label({ text: lightOn ? 'Turn off' : 'Turn on' });
+      toggleButton.add_child(toggleLabel);
+      this.menu.addMenuItem(toggleButton);
+      toggleButton.connect('activate', () => {
+        log ('Clicked brightness item');
+        toggleLight();
+      });
+
+      // Add UI elements
+      let brightnessPanel = new PopupMenu.PopupBaseMenuItem({activate: false});
+      brightnessPanel.add_child(new St.Label({text: 'Brightness: '}));
+      let brightnessSlider = new Slider.Slider(0);
+      brightnessPanel.add_child(brightnessSlider);
+      this.menu.addMenuItem(brightnessPanel);
+      brightnessSlider.connect('drag-end', () => {
+        updateBrightness(brightnessSlider.value * 100);
+      })
+
+      let temperaturePanel = new PopupMenu.PopupBaseMenuItem({activate: false});
+      temperaturePanel.add_child(new St.Label({text: 'Temperature: '}));
+      let temperatureSlider = new Slider.Slider(0);
+      temperaturePanel.add_child(temperatureSlider);
+      this.menu.addMenuItem(temperaturePanel);
+
+      this.menu.connect('open-state-changed', (menu, open) => {
+        if(open) {
+          log ('opened menu');
+        } else {
+          log ('closed menu');
+        }
+      });
+    }
+  }
+);
 
 // Get the saved settings from the light
 function getLightSettingsFromSchema() {
@@ -36,12 +88,7 @@ function getLightSettingsFromSchema() {
   return new Gio.Settings({ settings_schema: schemaObj });
 }
 
-function buildUI() {
-  let brightness = new PopupMenu.PopupMenuItem('Brightness');
-  brightness.add_child(new St.Label({ text: 'Brightness: ' + settings.get_int('bright') }));
-  indicatorButton.menu.addMenuItem(brightness);
-}
-
+// Toggle the light on or off
 function toggleLight() {
 
   // Run `journalctl -f -o cat /usr/bin/gnome-shell in terminal window to view log output
@@ -49,44 +96,38 @@ function toggleLight() {
   if (lightOn) {
     lightOn = false;
     GLib.spawn_command_line_sync('keylight-control --bright 0');
-    indicatorButtonText.set_text('On');
+    toggleLabel.set_text("Turn on");
   } else {
     lightOn = true;
-    GLib.spawn_command_line_sync('keylight-control --bright 50');
-    indicatorButtonText.set_text('Off');
+    GLib.spawn_command_line_sync('keylight-control --bright ' + settings.get_int('bright'));
+    toggleLabel.set_text("Turn off");
   }
+}
 
+// Update the brightness of the light 
+function updateBrightness(level) {
+  if (!lightOn) {
+    lightOn = true;
+    toggleLabel.set_text("Turn off");
+  }
+  GLib.spawn_command_line_sync('keylight-control  --bright ' + level.toFixed(0));
+  settings.set_int('bright', level.toFixed(0));
 }
 
 function init() {
   // Load the settings file from extension schema
   settings = getLightSettingsFromSchema();
   log('Light IP Address: ' + settings.get_string('light-ip-address'));
+  log('Light Brightness: ' + settings.get_int('bright'));
 }
 
 function enable() {
-    // Create a toggle button with "Elgato" text
-  // TODO: Make panel with additional controls
-  // For now, just a toggle
-  indicatorButton = new PanelMenu.Button({
-    style_class: 'panel-button'
-  });
-  indicatorButtonText = new St.Label({
-    text: 'Elgato',
-    y_align: Clutter.ActorAlign.CENTER,
-  });
-
-  //Make button respond as a toggle, add text, connect press event
-  //indicatorButton.set_toggle_mode(true);
-  indicatorButton.add_child(indicatorButtonText);
-  indicatorButton.connect('button-press-event', toggleLight); 
-
-
+  uiWindow = new UIWindow();
   // Add the button to the panel
-  Main.panel.addToStatusArea('indicator', indicatorButton, 1);
+  Main.panel.addToStatusArea('uiWindow', uiWindow, 1);
 }
 
 function disable() {
   // Remove the added button from panel
-  indicatorButton.destroy();
+  uiWindow.destroy();
 }
